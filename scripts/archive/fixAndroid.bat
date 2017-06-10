9609e8e516f19ee3b429ceec2825991c7cbfae53
Mark <mark@markhaney.net>
2017-06-10 07:23:37 -0700

    remove

--- scripts/archive/fixAndroid.bat
+++
@@ -1,6 +0,0 @@
-rem cordova plugin rm cordova-plugin-keyboard
-rem cordova plugin add cordova-plugin-keyboard
-rem cordova plugin rm cordova-plugin-splashscreen
-call cordova plugin add cordova-plugin-splashscreen
-call cordova plugin rm cordova-plugin-statusbar
-call cordova plugin add cordova-plugin-statusbar
